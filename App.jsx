10d8bf8c7d47a73e2466506f10d8e1feb63d5f2b
camisbersann <camila.bersan@aluno.senai.br>
2024-03-06 09:10:19 -0300

    feat: add tab icons styles

--- App.jsx
+++ App.jsx
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './src/screens/Home';
+import Profile from './src/screens/Profile';
+import Contact from './src/screens/Conatct';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -9,7 +13,27 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Home" component={Home} 
+       options={{
+        tabBarLabel: 'Início',
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name="home" color={452831} size={32} />
+        ),
+      }} />
+        <Tab.Screen name="Profile" component={Profile} 
+        options={{
+          tabBarLabel: 'Perfil',
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="account" color={452831} size={32} />
+          ),
+        }}/>
+        <Tab.Screen name="Contact" component={Contact}
+        options={{
+          tabBarLabel: 'Contato',
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="phone" color={452831} size={32} />
+          ),
+        }}/>
       </Tab.Navigator>
 
       <StatusBar style='auto'/>
