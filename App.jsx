518759a504ca757a988a7252cefb03e345236067
camisbersann <camila.bersan@aluno.senai.br>
2024-03-13 19:06:37 -0300

    feat: styles

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './src/screens/Home';
 import Profile from './src/screens/Profile';
 import Contact from './src/screens/Conatct';
+import { Feather } from "@expo/vector-icons";
 
 
 const Tab = createBottomTabNavigator();
@@ -15,27 +15,46 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home} 
        options={{
-        tabBarLabel: 'Início',
-        tabBarIcon: () => (
-          <MaterialCommunityIcons name="home" color={452831} size={32} />
+        tabBarIcon: ({ focused }) => (
+          <Feather
+            name="home"
+            size={24}
+            color={focused ? "#131313" : "#d6d6d6"}
+          />
         ),
+        tabBarLabel: "Home",
+        tabBarActiveTintColor: "#131313",
+        tabBarInactiveTintColor: "#777",
       }} />
+
         <Tab.Screen name="Profile" component={Profile} 
         options={{
-          tabBarLabel: 'Perfil',
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="account" color={452831} size={32} />
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="user"
+              size={24}
+              color={focused ? "#131313" : "#d6d6d6"}
+            />
           ),
-          
+          tabBarLabel: "Perfil",
+          tabBarActiveTintColor: "#131313",
+          tabBarInactiveTintColor: "#777",
         }}/>
+
         <Tab.Screen name="Contact" component={Contact}
         options={{
-          tabBarLabel: 'Contato',
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="phone" color={452831} size={32} />
-          ),
-          tabBarBadge: 3,
-        }}/>
+        tabBarIcon: ({ focused }) => (
+          <Feather
+            name="phone"
+            size={24}
+            color={focused ? "#131313" : "#d6d6d6"}
+          />
+        ),
+        tabBarLabel: "Perfil",
+        tabBarActiveTintColor: "#131313",
+        tabBarInactiveTintColor: "#777",
+        tabBarBadge: 3,
+      }}/>
       </Tab.Navigator>
 
       <StatusBar style='auto'/>
